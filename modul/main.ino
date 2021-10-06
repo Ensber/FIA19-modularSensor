0a99cdfdd5054b93f5db96eeb73c7d434c54c38b
KaffeeMaus <73712125+KaffeeMaus@users.noreply.github.com>
2021-10-06 14:56:23 +0200

    spi com for arduino uno

--- modul/main.ino
+++ modul/main.ino
@@ -1,9 +1,32 @@
-void setup()
-{
+#include "SPI.h"
+char buff [50];
+volatile byte indx;
+volatile boolean process;
 
+void setup (void) {
+   Serial.begin (9600);
+   Serial.write("start");
+   pinMode(MISO, OUTPUT); // have to send on master in so it set as output
+   SPCR |= _BV(SPE); // turn on SPI in slave mode
+   indx = 0; // buffer empty
+   process = false;
+   SPI.attachInterrupt(); // turn on interrupt
 }
 
-void loop()
-{
-    
+ISR (SPI_STC_vect) // SPI interrupt routine 
+{ 
+   byte c = SPDR; // read byte from SPI Data Register
+   if (indx < sizeof buff) {
+      buff [indx++] = c; // save data in the next index in the array buff
+      if (c == '\r') //check for the end of the word
+      process = true;
+   }
+}
+
+void loop (void) {
+   if (process) {
+      process = false; //reset the process
+      Serial.println (buff); //print the array on serial monitor
+      indx= 0; //reset button to zero
+   }
 }

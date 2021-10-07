87e348e7621de9abc090fb2ecb20419e2ade3eaa
KaffeeMaus <73712125+KaffeeMaus@users.noreply.github.com>
2021-10-07 13:16:57 +0200

    SPI communication splitted

--- modul/main.ino
+++ modul/main.ino
@@ -1,31 +1,44 @@
 #include "SPI.h"
+
 char buff [50];
 volatile byte indx;
-volatile boolean process;
+volatile boolean byteResieved;
 
-void setup (void) {
-   Serial.begin (9600);
-   Serial.write("start");
+void initSPICom()
+{
    pinMode(MISO, OUTPUT); // have to send on master in so it set as output
    SPCR |= _BV(SPE); // turn on SPI in slave mode
    indx = 0; // buffer empty
-   process = false;
+   byteResieved = false;
    SPI.attachInterrupt(); // turn on interrupt
 }
 
-ISR (SPI_STC_vect) // SPI interrupt routine 
-{ 
+void setup (void) {
+   Serial.begin (9600);
+   initSPICom();
+}
+
+void spiSend(int8_t p_byteToSend)
+{
+   SPI.transfer(p_byteToSend);
+}
+
+void handleResieve()
+{
    byte c = SPDR; // read byte from SPI Data Register
-   if (indx < sizeof buff) {
-      buff [indx++] = c; // save data in the next index in the array buff
-      if (c == '\r') //check for the end of the word
-      process = true;
-   }
+   //wann ist denn ein "Datensatz" angekommen
+   Serial.println(c);
+   spiSend(200);
+   
+}
+
+ISR (SPI_STC_vect) // SPI interrupt routine 
+{
+   handleResieve();
 }
 
 void loop (void) {
-   if (process) {
-      process = false; //reset the process
+   if (byteResieved) {
       Serial.println (buff); //print the array on serial monitor
       indx= 0; //reset button to zero
    }
